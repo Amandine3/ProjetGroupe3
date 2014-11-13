4a9578b37cad8363133184b2419e6d0bdf4a969a
Amandine3 <etu16751@yahoo.fr>
2014-11-13 17:17:44 +0100

    TOUT CE QU'ON A POUR L'INSTANT (tout rassemblé Aurélien & Amandine)

---
+++ src/ClassesDB/Room.java
@@ -0,0 +1,70 @@
+package ClassesDB;
+
+public class Room {
+	
+	/**
+	 * L'identifiant de la room
+	 */
+	private int idRoom;
+	/**
+	 * Le pseudo du créateur
+	 */
+	private String createur;
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public Room(){
+	}
+	
+	/**
+	 * Constructeur parametré
+	 * @param createur le pseudo du créateur
+	 */
+	 public Room(String createur)
+	 {
+		 this.createur=createur;
+	 }
+	
+	 /**
+	  * Second constructeur parametré
+	  * @param createur le pseudo du créateur
+	  * @param idroom l'identifiant de la room
+	  */
+	 public Room(int idRoom, String createur){
+		 this.idRoom=idRoom;
+		 this.createur=createur;
+	 }
+	 
+	/**
+	 * getter du createur
+	 * @return le pseudo du créateur
+	 */
+	public String getCreateur(){
+		return this.createur;
+	}
+	
+	/**
+	 * getter de l'idroom
+	 * @return l'identifiant de la room
+	 */
+	public int getIdRoom(){
+		return this.idRoom;
+	}
+
+	/**
+	 * Setter du créateur
+	 * @param createur le nouveau createur
+	 */
+	public void setCreateur(String createur){
+		this.createur=createur;
+	}
+	
+	/**
+	 * Setter de l'idRoom
+	 * @param identifiant le nouvel idRoom
+	 */
+	public void setIdRoom(int idRoom){
+		this.idRoom=idRoom;
+	}
+}
---
+++ src/ClassesDB/RoomDB.java
@@ -0,0 +1,26 @@
+package ClassesDB;
+
+public class RoomDB extends Room implements CRUD{
+	
+	public RoomDB(){
+		
+	}
+	public RoomDB(String createur){
+		super(createur);
+	}
+	public RoomDB(int idRoom, String createur){
+		super(idRoom, createur);
+	}
+	public void create(){
+		
+	}
+	public void read(){
+		
+	}
+	public void update(){
+		
+	}
+	public void delete(){
+		
+	}
+}
