643bd7dad15da8e5f9da6a262d7990a9e78dd1d4
VandaeleAurelien <aurelien.vandaele@condorcet.be>
2014-11-19 10:05:25 +0100

    RoomDB finis

--- src/ClassesDB/RoomDB.java
+++ src/ClassesDB/RoomDB.java
@@ -42,17 +42,66 @@ public class RoomDB extends Room implements CRUD
 	public RoomDB(int idRoom, String createur){
 		super(idRoom, createur);
 	}
-	public void create(){
-		String query= ";      
+	public void create() throws Exception{
+		CallableStatement cstmt =null;
+		try{
+			String query= "call createRoom1(?,?)";
+			cstmt = dbConnect.prepareCall(query)
+			cstmt.setString(1,createur);
+			cstmt.registerOutParameter(2, java.sql.Types.INTEGER);
+			cstmt.execute();
+			this.idRoom=cstmt.getInt(2);
+		}
+		catch(Exception e){
+            throw new Exception("Erreur lors de la création"+e.getMessage());
+        }
+        finally{
+            try{
+                cstmt.close();
+            }
+            catch(Exception e){}
+        }	
 	}
-	public void read(){
-		
+	public void read() throws Exception{
+		string query="select createur from room where idroom="+this.idRoom;
+		PreparedStatement cstmt=null;
+		try{
+            	cstmt = dbConnect.prepareStatement(query);
+            	ResultSet rs=cstmt.executeQuery(query);
+            	while(rs.next()){
+            		this.createur=rs.getString("createur");
+            	}
+        }
+        catch(Exception e){
+            throw new Exception("Erreur lors de la lecture"+e.getMessage());
+        }
+        finally{
+            try{
+                cstmt.close();
+            }
+            catch (Exception e){}
+        }
 	}
 	public void update()
 	{
 		// pas utilisable dans le cadre de cette application
 	}
 	public void delete(){
-		
+		CallableStatement cstmt =null;
+		try{
+			String query= "deleteRoom(?)";
+			cstmt = dbConnect.prepareCall(query)
+			cstmt.setString(1,this.idRoom);
+			cstmt.execute();
+		}
+		catch(Exception e){
+            throw new Exception("Erreur lors de la Suppression"+e.getMessage());
+        }
+        finally{
+            try{
+                cstmt.close();
+            }
+            catch(Exception e){}
+        }		
 	}
 }
