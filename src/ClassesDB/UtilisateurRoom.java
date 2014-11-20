b257b2e8d77475c33aec6baa3c95ba4708e5b360
VandaeleAurelien <aurelien.vandaele@condorcet.be>
2014-11-20 17:40:47 +0100

    Correction diverses et fin de utilisateurroom et utilisateurroomdb

--- src/ClassesDB/UtilisateurRoom.java
+++ src/ClassesDB/UtilisateurRoom.java
@@ -7,5 +7,41 @@ package ClassesDB;
 
 public class UtilisateurRoom
 {
+	
+	protected int idUtilisateurRoom, idRoom;
+	protected String pseudo;
+	
+	public UtilisateurRoom(){
+	}
+	
+	public UtilisateurRoom(int idUtilisateurRoom, int idRoom, String pseudo){
+		this.idUtilisateurRoom=idUtilisateurRoom;
+		this.idRoom=idRoom;
+		this.pseudo=pseudo;
+	}
+	
+	public String getPseudo(){
+		return this.pseudo;
+	}
+	
+	public int getIdUtilisateurRoom(){
+		return this.idUtilisateurRoom;
+	}
+	
+	public int getIdRoom(){
+		return this.idRoom;
+	}
+	
+	public void setPseudo(String pseudo){
+		this.pseudo=pseudo;
+	}
+	
+	public void setIdUtilisateurRoom(int idUtilisateurRoom){
+		this.idUtilisateurRoom=idUtilisateurRoom;
+	}
+	
+	public void setIdRoom(int idRoom){
+		this.idRoom=idRoom;
+	}
 
 }
--- src/ClassesDB/UtilisateurRoomDB.java
+++ src/ClassesDB/UtilisateurRoomDB.java
@@ -36,20 +36,76 @@ public class UtilisateurRoomDB extends UtilisateurRoom implements CRUD{
 		
 	}
 	
+	public UtilisateurRoomDB(int idUtilisateurRoom, int idRoom, String pseudo){
+		super(idUtilisateurRoom, idRoom, pseudo);
+	}
+	
 	/**
 	* enregistrement d'un nouvel utilisateurRoom dans la base de données
 	* @throws Exception erreur lors de la création
 	*/
-	public void create(){
-		
+	public void create() throws Exception{
+		CallableStatement cstmt=null;
+		try
+		{          
+			 String query="call createUtilisateurRoom(?,?)"; 
+			 cstmt = dbConnect.prepareCall(query);
+			 cstmt.setInt(1, this.idRoom);
+			 cstmt.setString(2,this.pseudo);
+			cstmt.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			throw new Exception("Erreur lors de la création"+e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				cstmt.close();
+			}
+			catch(Exception e)
+			{
+				
+			}
+		}
 	}
 	
 	/**
 	* récupération des données d'un utilisateurRoom sur base de son 
 	* @throws Exception code inconnu
 	*/
-	public void read(){
+	public void read() throws Exception{
+		String query = "SELECT * FROM UtilisateurRoom WHERE idUtilisateurRoom=?";
+		PreparedStatement cstmt=null;
+		try
+		{
+			cstmt = dbConnect.prepareStatement(query);
+			cstmt.setInt(1,this.idUtilisateurRoom);
+			ResultSet rs=cstmt.executeQuery(query);
+			while(rs.next())
+			{
+				this.idRoom=rs.getInt("IDROOM");
+				this.pseudo=rs.getString("PSEUDO");
+			}
 		
+		}
+		catch(Exception e)
+		{
+			throw new Exception("Erreur lors de la lecture"+e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+			   cstmt.close();
+			}
+			catch (Exception e)
+			{
+				
+			}
+		}
+	
 	}
 	
 	/**
@@ -64,7 +120,60 @@ public class UtilisateurRoomDB extends UtilisateurRoom implements CRUD{
      * suppression de l'utilisateurroom sur base de son 
      * @throws Exception erreur lors de la suppression
      */
-	public void delete(){
-		
+	public void delete() throws Exception{
+		CallableStatement cstmt=null;
+		try
+		{          
+			 String query="call deleteUtilisateurRoom(?,?)"; 
+			 cstmt = dbConnect.prepareCall(query);
+			 cstmt.setInt(1, this.idRoom);
+			 cstmt.setString(2,this.pseudo);
+			cstmt.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			throw new Exception("Erreur lors de la suppression"+e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				cstmt.close();
+			}
+			catch(Exception e)
+			{
+				
+			}
+		}
 	}
+	
+	public static ArrayList<int> readRoom(String pseudo) throws Exception{
+		PreparedStatement cstmt=null;
+		ArrayList<int> resultat=new ArrayList();
+		try{
+			string query="select idRoom from UtilisateurRoom where pseudo=?";
+			cstmt=dbConnect.prepareCall(query);
+			cstmt.setString(1, pseudo);
+			ResultSet rs=cstmt.executeQuery(query);
+			while(rs.next())
+			{
+				resultat.add(rs.getInt("IDROOM"));
+			}
+			return resultat;
+		}
+		catch(Exception e)
+		{
+			throw new Exception("Erreur lors de la lecture"+e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+			   cstmt.close();
+			}
+			catch (Exception e)
+			{
+				
+			}
+		}
 }
