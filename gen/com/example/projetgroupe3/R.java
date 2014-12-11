e683d15412c76a17ec8333686f12772c1c9aa362
Amandine3 <etu16751@yahoo.fr>
2014-12-11 11:45:35 +0100

    a regarder : listeRoomUtilisateur

--- gen/com/example/projetgroupe3/R.java
+++ gen/com/example/projetgroupe3/R.java
@@ -1109,12 +1109,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int boutonA=0x7f05003c;
         public static final int boutonB=0x7f05003d;
+        public static final int boutonVal=0x7f050044;
         public static final int button1=0x7f05003e;
         public static final int button2=0x7f05003f;
         public static final int checkbox=0x7f05002d;
@@ -1278,6 +1279,7 @@ containing a value of this type.
         public static final int boutonAnnuler=0x7f0a0013;
         public static final int boutonB=0x7f0a001e;
         public static final int boutonOk=0x7f0a0012;
+        public static final int boutonVal=0x7f0a0021;
         public static final int button1=0x7f0a0016;
         public static final int contsms=0x7f0a001b;
         public static final int hello_world=0x7f0a000e;
