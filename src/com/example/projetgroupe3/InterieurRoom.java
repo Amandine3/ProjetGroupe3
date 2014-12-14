9fa1792dba4fa99b1da754ff0e7a60d7c8ab4381
VandaeleAurelien <aurelien.vandaele@condorcet.be>
2014-12-14 10:12:09 +0100

    Manque plus que détails

--- src/com/example/projetgroupe3/InterieurRoom.java
+++ src/com/example/projetgroupe3/InterieurRoom.java
@@ -7,9 +7,13 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import myconnections.DBConnection;
+
 import com.example.projetgroupe3.ListeRoomUtilisateur.MyAccesDB3;
 
 import ClassesDB.MessageDB;
+import ClassesDB.RoomDB;
+import ClassesDB.UtilisateurRoomDB;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -17,6 +21,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.view.View.OnClickListener;
@@ -26,8 +31,14 @@ public class InterieurRoom extends ActionBarActivity{
 	private Connection con;
 	private ListView list=null;
 	private Button envoi=null;
-	Envoi en;
-	
+	private Envoi e;
+	private Reception r;
+	private ArrayList<MessageDB> mess;
+	private String resultat;
+	private ArrayList<String> liste;
+	private String pseudo;
+	private int idroom;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -35,24 +46,25 @@ public class InterieurRoom extends ActionBarActivity{
 		Log.d("AAA","AAA");
 		setContentView(R.layout.activity_liste_roomut);
 		Log.d("BBB","BBB");
-		en = new Envoi(InterieurRoom.this);
-		new Timer().schedule(new TimerTask(){
-		public void run(){
-			en.execute();
-		}
-		}, new Date(), 20000);
+		//récupérer les pseudo et idroom
+		r = new Reception(InterieurRoom.this);
 		envoi=(Button)findViewById(R.id.boutonenvoi);
 		envoi.setOnClickListener(
 				new OnClickListener(){
 
 					@Override
 					public void onClick(View v) {
-						MessageDB mess=new MessageDB();
-						mess.create();
-						
+						Envoi e=new Envoi(InterieurRoom.this);
+						e.execute();
 					}
 					
 				});
+		new Timer().schedule(new TimerTask(){
+		public void run(){
+			r.execute();
+		}
+		}, new Date(), 20000);
+
 	}
 
 	@Override
@@ -108,9 +120,79 @@ public class InterieurRoom extends ActionBarActivity{
 		}
 		@Override
 		protected Boolean doInBackground(String... params) {
-			// TODO Auto-generated method stub
+            if(con==null)
+            {
+                    con = new DBConnection().getConnection();
+                    if(con==null)
+                    {
+                            resultat = "ECHEC de la connexion";
+                            Log.d("Con","Problème Connexion");
+                            return false;
+                    }
+                    Log.d("Con","Connexion OK");
+
+                    MessageDB.setConnection(con);
+            
+            }
+			MessageDB mess=new MessageDB("Ce qu'il y a dans le textfield", idroom, pseudo);
+			try{
+				mess.create();
+			}
+			catch(Exception e){
+				//creer un toast disant qu'il y a eu un probleme lors de l'envoi du message
+			}
 			return null;
 		}
 	}
+	class Reception extends AsyncTask<String,Integer,Boolean>{
+		public Reception(InterieurRoom pActivity)
+		{
+
+			link(pActivity);
+			// TODO Auto-generated constructor stub
+		}
+
+		private void link(InterieurRoom pActivity)
+		{
+			// TODO Auto-generated method stub
+
+
+		}
+		@Override
+		protected Boolean doInBackground(String... params) {
+            if(con==null)
+            {
+                    con = new DBConnection().getConnection();
+                    if(con==null)
+                    {
+                            resultat = "ECHEC de la connexion";
+                            Log.d("Con","Problème Connexion");
+                            return false;
+                    }
+                    Log.d("Con","Connexion OK");
+
+                    RoomDB.setConnection(con);
+            
+            }
+			try{
+				mess=RoomDB.getMessageRoom(idroom);
+				for (int i=0;i<mess.size();i++){
+					//Faire une meilleure mise en page
+					liste.add(mess.get(i).getPseudo()+mess.get(i).getContenu());
+				}
+			}
+			catch(Exception e){
+				//creer un toast disant qu'il y a eu un probleme lors de l'envoi du message
+			}
+			return null;
+		}
+		protected void onPostExecute(){
+			list=(ListView)findViewById(R.id.listView1);
+			//faire que la liste s'affiche depuis la fin -_- et faire qu'on peut pas cocher
+			//Manque la redirection dans listeRoomUtilisateur et peut être changer les contenus des resultat afin de les traduire :'(
+			ArrayAdapter<String> adapter = new  ArrayAdapter<String>(InterieurRoom.this,android.R.layout.simple_list_item_single_choice, liste);
+			list.setAdapter(adapter);
+		}
 	}
+
 }
